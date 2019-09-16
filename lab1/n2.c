c550d3589deff6f1adeaa9a246fffd06be0f99f7
Святослав Никитин <moonlight@mbp-moonlight.elvees.com>
2019-09-16 09:40:28 +0300

    i

--- lab1/n2.c
+++ lab1/n2.c
@@ -1,10 +1,34 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 #include <errno.h>
+#include <string.h>
 int main(int argc, char * argv[]){
+    if(argc == 1){
+        printf("No arguments!\n");
+    }
+    else if(argc == 2){
+        printf("No permission options!\n");
+    }
+    else {
+        printf("Creating file %s with permissions %s\n", argv[1], argv[2]);
+        FILE *fp = fopen(argv[1], "w");
+        int code;
+        char str[255];
+        do {
+            code = scanf("%s", str);
+            if(code != -1){
+                fwrite(&str, sizeof(char), strlen(str), fp);
+                fwrite("\n", sizeof(char), 1, fp);
+            }
 
-    for(int i = 0; i < argc; i++) {
-        printf("%s \n", argv[i]);
+        } while(code != -1);
+        int mode = strtol(argv[2],0,8);
+        fclose(fp);
+        if(chmod(argv[1], mode) < 0){
+            perror("chmod error: ");
+        }
     }
     return 0;
 }
