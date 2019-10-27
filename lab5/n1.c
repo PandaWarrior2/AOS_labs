f479a7187442850651f03e21893bd23e426765d4
Святослав Никитин <SANikitin@mephi.ru>
2019-10-27 13:22:36 +0300

    add number 1

---
+++ lab5/n1.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  n1.c
+ *
+ *    Description:  Lab 5, number 1
+ *
+ *        Version:  1.0
+ *        Created:  27.10.2019 13:16:57
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Nikitin Svyatoslav, M19-512
+ *   Organization:  MEPhI
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <mqueue.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+int main(int argc, char * argv[]){
+
+}
+
