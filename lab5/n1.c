447d79b518c9d4be94c7e8a37e5ff7fef0a127f3
PandaWarrior2 <stasmakaveli@yandex.ru>
2019-10-28 14:33:21 +0300

    upd

--- lab5/n1.c
+++ lab5/n1.c
@@ -20,7 +20,22 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
 int main(int argc, char * argv[]){
-
+    int qd;
+    struct mq_attr attr;
+    if((qd = mq_open("/queue", O_RDWR | O_CREAT, 0755, NULL)) == -1){
+        perror("mq_open");
+        exit(1);
+    }
+    if(mq_getattr(qd, &attr) == -1){
+        perror("mq_getattr");
+        exit(1);
+    }
+    printf("Message queue stats:\n");
+    printf("Flags: %d\nMax messages: %d\nMax message size: %d\nMessages count: %d\n", attr.mq_flags, attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
+    return 0;
 }
 
