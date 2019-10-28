41be423bf04812a38439867d7d341edaf9415b35
Никитин Святослав Александрович <sanikitin@andrastea-pc.elvees.com>
2019-10-28 15:40:30 +0300

    Add numbers 1,2,3. Update makefile

--- lab5/n1.c
+++ lab5/n1.c
@@ -16,26 +16,83 @@
  * =====================================================================================
  */
 #include <stdlib.h>
-#include <mqueue.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+int get_stats(int);
+void gen_random(char *, const int);
 int main(int argc, char * argv[]){
     int qd;
-    struct mq_attr attr;
-    if((qd = mq_open("/queue", O_RDWR | O_CREAT, 0755, NULL)) == -1){
+    /*if((qd = mq_open("/queue", O_RDWR | O_CREAT, 0755, NULL)) == -1){
         perror("mq_open");
         exit(1);
-    }
-    if(mq_getattr(qd, &attr) == -1){
-        perror("mq_getattr");
+    }*/
+    if((qd = msgget(1, IPC_CREAT | 0755)) == -1) {
+        perror("msgget");
         exit(1);
     }
-    printf("Message queue stats:\n");
-    printf("Flags: %d\nMax messages: %d\nMax message size: %d\nMessages count: %d\n", attr.mq_flags, attr.mq_maxmsg, attr.mq_msgsize, attr.mq_curmsgs);
+    struct msqid_ds qds;
+    get_stats(qd);
+    struct msg {
+        long mtype;
+        char mtext[20];
+    } msg;
+
+    char str[20];
+    for(int i = 0; i < 50; i++){
+        msg.mtype = (long)i;
+        gen_random(str, 10);
+        strcpy(msg.mtext, str);
+        if(msgsnd(qd, &msg, 20, 0) == -1){
+            perror("msgsnd");
+        }
+        printf("Message send! \n");
+    }
+    get_stats(qd);
+    //it's number 2
+    /*printf("Recieve messages...\n");
+    for(int i = 0; i < 20; i++) {
+        if(msgrcv(qd, &msg, 20, 0, IPC_NOWAIT) == -1) {
+            perror("msgrcv");
+        }
+        else{
+            printf("Message recieved.  mtype = %d, mtext = %s\n", msg.mtype, msg.mtext);
+        }
+    }
+    */
     return 0;
 }
 
+int get_stats(int qd){
+    struct msqid_ds qds;
+    if(msgctl(qd, IPC_STAT, &qds) == -1){
+        perror("msgctl");
+        return -1;
+    }
+    printf("UserID = %d\n", qds.msg_perm.uid);
+    printf("GroupID = %d\n", qds.msg_perm.gid);
+    printf("CTime = %d\n", qds.msg_ctime);
+    printf("Max bytes = %d\n", qds.msg_qbytes);
+    printf("Number of messages: %d\n", qds.msg_qnum);
+    return 0;
+}
+
+void gen_random(char *s, const int len) {
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < len; ++i) {
+        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+
+    s[len] = 0;
+}
---
+++ lab5/n3.c
@@ -0,0 +1,55 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  n3.c
+ *
+ *    Description:  Lab 5. number 3.
+ *
+ *        Version:  1.0
+ *        Created:  10/28/2019 03:36:53 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Svyatoslav Nikitin, M19-512
+ *   Organization:  MEPhI
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+int main(int argc, char * argv[]){
+    if(argc < 3){
+        printf("Usage: %s queue_id msg_type\n", argv[0]);
+        exit(1);
+    }
+    int msg_type = strtol(argv[2], NULL, 10);
+    int qid = strtol(argv[1], NULL, 10);
+    int qd;
+
+    struct msg {
+        long mtype;
+        char mtext[20];
+    } msg;
+
+    if((qd = msgget(qid, IPC_CREAT | 0755)) == -1) {
+        perror("msgget");
+        exit(1);
+    }
+
+    if(msgrcv(qd, &msg, 20, msg_type, IPC_NOWAIT) == -1) {
+        perror("msgrcv");
+    }
+    else{
+        printf("Message recieved.  mtype = %d, mtext = %s\n", msg.mtype, msg.mtext);
+    }
+    return 0;
+}
