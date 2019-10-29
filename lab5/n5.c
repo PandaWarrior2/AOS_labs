73962eaefa86e3a701ae59e6d51a34a960b757dc
PandaWarrior2 <stasmakaveli@yandex.ru>
2019-10-29 09:28:42 +0300

    Update number 5

--- lab5/n5.c
+++ lab5/n5.c
@@ -10,7 +10,7 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
+ *         Author:  Svyatoslav Nikitin, M19-512
  *   Organization:  MEPhI
  *
  * =====================================================================================
@@ -28,15 +28,19 @@
 #include <string.h>
 int main(int argc, char * argv[]){
     if(argc < 2){
-        printf("Usage: %s queue_id\n", argv[0]);
+        printf("Usage: %s key\n", argv[0]);
         exit(1);
     }
 
-    int qid =  strtol(argv[1], NULL, 10);
-    if(msgctl(qid, IPC_RMID, 0) == -1){
-        perror("");
+    int key =  strtol(argv[1], NULL, 10);
+    int qid;
+    if((qid = msgget(key, 0755)) == -1){
+        perror("msgget");
+    }
+    if(msgctl(qid, IPC_RMID, NULL) == -1){
+        perror("msgctl");
         exit(1);
     }
-    printf("Queue %d deleted!\n", qid);
+    printf("Queue with key %d deleted!\n", key);
     return 0;
 }
