1ccfb0576636d83277503435dac0de0bb1d26d64
Никитин Святослав Александрович <sanikitin@andrastea-pc.elvees.com>
2019-10-31 14:37:52 +0300

    del comments:

--- lab5/n5.c
+++ lab5/n5.c
@@ -28,10 +28,6 @@
 #include <string.h>
 static void usage(char*, char*);
 int main(int argc, char * argv[]){
-    /*if(argc < 2){
-        printf("Usage: %s key\n", argv[0]);
-        exit(1);
-    }*/
     int mode=0, opt;
     key_t key;
     while((opt = getopt(argc, argv, "f:k:")) != -1){
